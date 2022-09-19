59d6fcb46a7212682f1aa0752c87cf904d4a8a34
Jade Nguyen <jade.nguyen@zoopla.co.uk>
2022-09-19 20:49:20 +0100

    app working and rendering hello

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,26 +1,35 @@
+import { FC } from 'react';
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Listings from './pages/Listings/Listings';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p>
+					Hello <code>src/App.tsx</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
+
+// const App: FC = () => {
+// 	return (
+// 		<Routes>
+// 			<Route index element={<Listings />} />
+// 		</Routes>
+// 	);
+// };
 
 export default App;
---
+++ my-app/src/pages/Listings/Listings.tsx
@@ -0,0 +1,8 @@
+// import { FC } from "react";
+// import { Card } from '../../components/Card/Card';
+
+const Listings = () => {
+	return <p>Listings</p>;
+};
+
+export default Listings;
