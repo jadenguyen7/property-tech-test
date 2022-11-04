3b00638d01e98de9f7e25a07abc62bb799675aaf
Jade Nguyen <jade.nguyen@zoopla.co.uk>
2022-11-04 15:06:56 +0000

    fixed app to working state no errors

--- my-app/src/components/Card/Card.tsx
+++ my-app/src/components/Card/Card.tsx
@@ -1,42 +1,72 @@
 import { FC, useEffect, useState } from 'react'
+import { Listing } from '../../requests/listings.types'
 import styles from './Card.module.css'
 
 // GET /api/properties - return the full list of listings
-// POST /api/properties - create a new listing
 
 // GET /api/properties/id - return specific listing matching id
-// DELETE //api/properties/id - delete the licthing matching the id
+
+// an array of the id's
+// a function that passes in the id and updates the status property by making a post request
+
+type PropertyProps = {
+	id: number
+	image: string
+	address: string
+	bedrooms: string
+	price: string
+	status: boolean
+}
 
 const Card: FC = () => {
-	const [properties, setProperties] = useState({
-		items: [
-			{
-				image: '',
-				address: '',
-				bedrooms: '',
-				price: '',
-				status: '',
-			},
-		],
-	})
+	const [properties, setProperties] = useState([])
+	const [status, setStatus] = useState(true)
 
-	const getListings = () => {
-		fetch('/listings')
-			.then((res) => {
-				return res.json()
-			})
+	// const getListings = () => {
+	// 	fetch('/listings')
+	// 		.then((res) => {
+	// 			return res.json()
+	// 		})
 
-			.then((items) => setProperties({ items }))
+	// 		.then((items) => setProperties(items))
+	// }
+
+	// const getListings = async () => {
+	// 	const response = await fetch('/listings')
+	// 	const data = await response.json()
+	// 	setProperties(data)
+	// 	setStatus(data.status)
+	// }
+
+	// const getListingStatus = async () => {
+	// 	const response = await fetch('/listings')
+	// 	const data: GetListingResponse = await response.json()
+	// 	setStatus(data.status)
+	// }
+
+	const putStatus = async (id: number) => {
+		const response = await fetch('/listings', {
+			method: 'put',
+			body: JSON.stringify({ id: id, status: !status }),
+		})
+		const data: Listing = await response.json()
+		setStatus(data.status)
 	}
 
 	// to stop the infinitive loop
 	useEffect(() => {
+		const getListings = async () => {
+			const response = await fetch('/listings')
+			const data = await response.json()
+			setProperties(data)
+			setStatus(data.status)
+		}
 		getListings()
 	}, [])
 
 	return (
 		<>
-			{properties.items.map((property, i) => {
+			{properties.map((property: PropertyProps, i) => {
 				return (
 					<div className={styles.cardContainer} key={i}>
 						<div className={styles.card}>
@@ -50,7 +80,15 @@ const Card: FC = () => {
 								<h3>{property.address}</h3>
 								<p>{property.bedrooms}</p>
 								<p>{property.price}</p>
-								<p>{property.status}</p>
+
+								<input
+									type="checkbox"
+									id="status"
+									checked={status}
+									onChange={() => putStatus(property.id)}
+								/>
+
+								<p>{status ? 'Active' : 'Inactive'}</p>
 							</div>
 						</div>
 					</div>
